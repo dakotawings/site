4c806cdefa3507229aaf0966b66384f77abf7abf
dakotawings <42878677+dakotawings@users.noreply.github.com>
2019-03-14 05:43:38 +0530

    Update ppc-landing.php

--- ppc-landing.php
+++ ppc-landing.php
@@ -1,11 +1,16 @@
+---
+layout: ppc-landing
+title: PPC landing
+permalink: /ppc-landing/
+---
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="viewport" content="width=device-width, initial-scale=1">	
-	<meta name="robots" content="noindex, nofollow">
-    <title>Dakota</title>
+    <meta name="robots" content="noindex, nofollow">
+    <title>Dakota Wings - Citation Training, Mustang Training, Cessna CJ Training</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
