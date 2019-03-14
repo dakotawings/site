8be0e5b798112f7b4bf5432ddd58598c92c29e2d
dakotawings <42878677+dakotawings@users.noreply.github.com>
2019-03-14 05:36:47 +0530

    Create ppc-landing.php

---
+++ ppc-landing.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	<meta name="robots" content="noindex, nofollow">
+    <title>Dakota</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	<link href="css/ionicons.min.css" rel="stylesheet">
+	<link href="css/animate.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700|Poppins:100,200,300,400,500,700" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+<div class="main_wrapper">
+
+<!-- ====================Header =====================-->
+<header class="navbar navbar-static-top header" id="top" role="slider">
+	<div class="container">
+	<div class="navbar-header">
+	<a href="" class="navbar-brand"><img  src="images/logo.png" alt=""></a>
+	</div>	
+	
+	<div class="call-us"><i class="ion-android-call"></i>Call Us! <a href="tel:605-299-2204">605-299-2204</a>	
+	</div>
+	</div>
+</header>
+<!-- ====================Header END here =====================-->
+
+<!-- ====================Hero =====================-->
+
+<section class="hero" style="background:url('images/hero.jpg')">
+        <div class="container">		 
+		<div class="row">
+		     <div class="col-md-6 wow fadeIn col-sm-7">	
+			  <div class="hero-intro">
+			   <h1 class="text-left text-uppercase">The Best <span>Aviation Training </span><strong>You Can Get</strong></h1>
+			   <p>At Dakota Wings, we are pleased to offer initial or recurrent training in Citation Mustangs or any of the Cessna CJ series aircraft. Our instructors are all current licensed professional pilots and will tailor a course based on your needs and experience.</p>
+			   
+		     </div>	
+			 
+			 </div>	
+		
+		   <div class="contact-form col-md-4 col-md-offset-2 col-sm-5">	   
+			
+				<!-- Contact Form  -->
+				
+				<form class="form-horizontal wow fadeIn text-center" action="https://formspree.io/rasmussen4@swiftel.net" method="POST">
+				
+				   <div class="form-group text-center">
+				      <h3 class="title text-uppercase blue-underline">GET More Information</h3>	   
+                     </div>
+					 
+					 	<div class="form-group">
+									 <input class="form-control" placeholder="Your Name" name="Name" type="text">
+								</div>						
+								
+			
+				       <div class="form-group">
+								 <input class="form-control" placeholder="Phone Number" name="PhoneNumber" type="text">
+					        </div>  
+					
+								
+                        <div class="form-group">
+								 <input class="form-control" placeholder="Email Address" name="Email" type="email">
+					        </div>
+
+                           <div class="form-group">
+								 <textarea class="form-control" placeholder="Your Message" name="Message" rows="4"></textarea>
+					        </div>								
+					 
+					<div class="form-group">
+					<button class="btn btn-primary btn-lg btn-block" type="submit">SUBMIT</button>
+					</div>
+					
+					</form><!-- Contact Form END -->			
+			 </div>		
+				
+			 
+			
+			 
+			 
+			 
+			 
+			 
+		</div>
+		
+   </div><!----- / Container ---->
+</section>
+
+
+
+<section class="wow fadeIn">
+		<div class="container">
+
+		<div class="row">
+
+			<div class="col-sm-6">
+			  <div class="training-box">			  
+			  <div class="box-image">
+			         <h3 class="title text-uppercase blue-underline text-left">Citation Mustang Training</h3>
+			      <img src="images/citation-image.jpg"></div>  
+				 <div class="box-content"> 
+			     <p>Our focus is to provide you with training that is customized to your experience level. We offer training for PIC Single Pilot type ratings, PIC Crew type ratings, and SIC only type ratings. Our training is in aircraft training with your aircraft or ours.</p> 
+				 <div class="clearfix"></div>
+				 
+				 <h3 class="text-uppercase">Requirements</h3>
+				 <ul class="list arrow">
+					  <li>Current Medical</li>
+					  <li>Private, Commercial, or Airline Transport Pilot License</li>
+					  <li>Multi-Engine Class Rating</li>
+					  <li>Instrument Airplane Rating</li>
+					 </ul>			
+			     </div>
+			     </div>
+				 
+				 
+			   </div>
+			   
+			   
+			   <div class="col-sm-6">
+			  <div class="training-box">			  
+			  <div class="box-image">
+			         <h3 class="title text-uppercase blue-underline text-left">CJ Training</h3>
+			      <img src="images/cj-image.jpg"></div>  
+				 <div class="box-content"> 
+			     <p>Let our current professional pilots build a training schedule to fit your experience level in any of the CJ models. That includes, CJ, CJ1, CJ1+, CJ2, CJ2+, CJ3, CJ3+, CJ4, and M2. We offer training for PIC Single Pilot type ratings, PIC Crew type ratings, and SIC only type ratings.</p> 
+				 <div class="clearfix"></div>
+				 
+				 <h3 class="text-uppercase">Requirements</h3>
+				 <ul class="list arrow">
+					  <li>Private, Commercial, or Airline Transport Pilot License</li>
+					  <li>Multi-Engine Class Rating</li>
+					  <li>Instrument Airplane Rating</li>
+					 </ul>			
+			     </div>
+			     </div>
+				 
+				 
+			   </div>
+
+
+
+		</div>
+
+
+		</div>
+
+</section>
+
+
+
+<div class="copyright">&copy; 2019 Dakota Wings |  All Rights Reserved</div>
+
+
+  
+  </div><!------ ===========  main_wrapper Close  ==========------>
+  
+  
+  
+  
+  
+  
+ 
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script>
+	wow = new WOW({
+    boxClass: "wow",
+    animateClass: "animated",
+    offset: 0,
+    mobile: !0,
+    live: !0
+}), wow.init();
+	
+	</script>
+	
+  </body>
+</html>
